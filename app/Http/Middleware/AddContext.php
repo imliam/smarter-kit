a648c6622f96ca82ed183c3044b112232f0391ee
Liam Hammett <liamhammett@gmail.com>
2025-08-24 22:58:11 +0100

    Add context

---
+++ app/Http/Middleware/AddContext.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Context;
+use Symfony\Component\HttpFoundation\Response;
+
+class AddContext
+{
+    /** @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Context::add('url', $request->url());
+        Context::add('trace_id', Str::uuid()->toString());
+
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AddContext;
 use App\Http\Middleware\HttpsRedirect;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append([
+            AddContext::class,
             HttpsRedirect::class,
         ]);
     })
