51896ce7045fb45980324bc7f43da67a1964cd2a
Liam Hammett <liamhammett@gmail.com>
2025-08-24 16:22:42 +0100

    Set up Prettier and ESLint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+        },
+    },
+    {
+        ignores: ['vendor', 'node_modules', 'public', 'tailwind.config.js'],
+    },
+    prettier,
+];
