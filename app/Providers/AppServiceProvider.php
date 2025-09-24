7545125a99f162de93fe1ff4d8ea6e9d8a335a8f
Liam Hammett <liamhammett@gmail.com>
2025-09-24 20:42:12 +0100

    Unguard models

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
         Vite::useAggressivePrefetching();
         URL::forceHttps(app()->isProduction());
         DB::prohibitDestructiveCommands(app()->isProduction());
+        Model::unguard();
     }
 }
