72f3eedae3e3c31aecb7e8cbe68d8c5e1dcfa12b
Liam Hammett <liamhammett@gmail.com>
2026-04-15 02:25:23 +0100

    Ensure production is always HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
@@ -16,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        Vite::useAggressivePrefetching();
+        //
     }
 
     /**
@@ -32,6 +33,10 @@ class AppServiceProvider extends ServiceProvider
      */
     protected function configureDefaults(): void
     {
+        URL::forceHttps(app()->isProduction());
+
+        Vite::useAggressivePrefetching();
+
         Date::use(CarbonImmutable::class);
 
         DB::prohibitDestructiveCommands(
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\HttpsRedirect;
 use App\Http\Middleware\SetTeamUrlDefaults;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
+            HttpsRedirect::class,
             SetTeamUrlDefaults::class,
         ]);
     })
