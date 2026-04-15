e8e95df345b150aa6841e63e20a532f1d2d3374d
Liam Hammett <liamhammett@gmail.com>
2026-04-15 01:52:02 +0100

    Set up Prettier, ESLint and run linters across codebase

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+import tailwindcss from "@tailwindcss/vite";
+import laravel from 'laravel-vite-plugin';
 import {
     defineConfig
 } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
