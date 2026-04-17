45a0ec484bb0a9d9e050bda4c57e094daf7c7422
Liam Hammett <liamhammett@gmail.com>
2026-04-18 00:14:00 +0100

    Reference app.ts

--- resources/views/layouts/wrapper.blade.php
+++ resources/views/layouts/wrapper.blade.php
@@ -18,7 +18,7 @@
         rel="stylesheet"
     />
 
-    @vite (['resources/css/app.css', 'resources/js/app.js'])
+    @vite (['resources/css/app.css', 'resources/js/app.ts'])
     @fluxAppearance
 </head>
 <body {{ $attributes->except('title') }}>
