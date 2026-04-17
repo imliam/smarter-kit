0f39e5fc9366d055c209ecd5dbe5647f663119f7
Liam Hammett <liamhammett@gmail.com>
2026-04-18 00:38:12 +0100

    Set up more strict architecture tests

--- tests/ArchitectureTest.php
+++ tests/ArchitectureTest.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Models\Model;
+use Pest\Arch\Support\Composer;
 
 arch()->preset()->php();
 arch()->preset()->security();
@@ -10,3 +11,14 @@ arch()->preset()->laravel()
     ->ignoring([
         Model::class,
     ]);
+
+arch()->expect(['sleep', 'usleep'])->not->toBeUsed();
+
+arch('tests to not be used in application code')
+    ->expect('Tests')
+    ->not->toBeUsedIn('App');
+
+foreach (Composer::userNamespaces() as $namespace) {
+    arch()->expect($namespace)->toUseStrictTypes();
+    arch()->expect($namespace)->toUseStrictEquality();
+}
