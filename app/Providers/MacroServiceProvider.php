1ae71947acf8a90520b66e34a27d0cec34bfccc6
Liam Hammett <liamhammett@gmail.com>
2026-04-25 15:25:36 +0100

    Add MacroServiceProvider

---
+++ app/Providers/MacroServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MacroServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        //
+    }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,12 +1,8 @@
 <?php
 
-declare(strict_types=1);
-use App\Providers\AppServiceProvider;
-use App\Providers\Filament\AdminPanelProvider;
-use App\Providers\FortifyServiceProvider;
-
 return [
-    AppServiceProvider::class,
-    AdminPanelProvider::class,
-    FortifyServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    App\Providers\MacroServiceProvider::class,
 ];
