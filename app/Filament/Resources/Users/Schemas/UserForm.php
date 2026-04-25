748cbd1cfbd697c0765026eb619183693d2a7200
Liam Hammett <liamhammett@gmail.com>
2026-04-25 01:54:24 +0100

    Add a user resource to Filament

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use App\Filament\Resources\Users\Pages\CreateUser;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                TextInput::make('password')
+                    ->password()
+                    ->required(fn ($livewire): bool => $livewire instanceof CreateUser)
+                    ->revealable(filament()->arePasswordsRevealable())
+                    ->rule(Password::default())
+                    ->autocomplete('new-password')
+                    ->dehydrated(fn ($state): bool => filled($state))
+                    ->dehydrateStateUsing(fn ($state): string => Hash::make($state)),
+            ]);
+    }
+}
