47f441ed36766927bea57890e7b4a43cef5da742
Liam Hammett <liamhammett@gmail.com>
2026-04-29 23:08:47 +0200

    Code style fixes

--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -25,43 +25,31 @@ class FilamentServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Select::configureUsing(function (Select $field) {
-            return $field
-                ->searchable()
-                ->preload();
-        });
+        Select::configureUsing(fn(Select $field) => $field
+            ->searchable()
+            ->preload());
 
         // Add sensible min and max values to prevent dates like 01/01/0000 or 01/01/3000
-        DatePicker::configureUsing(function (DatePicker $datePicker) {
-            return $datePicker
-                ->minDate(Carbon::createFromDate(1500, 1, 1))
-                ->maxDate(now()->addYears(30));
-        });
+        DatePicker::configureUsing(fn(DatePicker $datePicker) => $datePicker
+            ->minDate(\Illuminate\Support\Facades\Date::createFromDate(1500, 1, 1))
+            ->maxDate(now()->addYears(30)));
 
-        Table::configureUsing(function (Table $table) {
-            return $table
-                ->striped()
-                ->deferLoading()
-                ->reorderableColumns()
-                ->columnManagerColumns(2)
-                ->columnManagerTriggerAction(fn(Action $action) => $action->button()->label('Columns'))
-                ->filtersTriggerAction(fn(Action $action) => $action->button()->label('Filters')->slideOver()->closeModalByClickingAway(true))
-                ->filtersFormWidth(Width::Small)
-                ->paginationPageOptions([10, 25, 50, 100]);
-        });
+        Table::configureUsing(fn(Table $table) => $table
+            ->striped()
+            ->deferLoading()
+            ->reorderableColumns()
+            ->columnManagerColumns(2)
+            ->columnManagerTriggerAction(fn(Action $action): \Filament\Actions\Action => $action->button()->label('Columns'))
+            ->filtersTriggerAction(fn(Action $action): \Filament\Actions\Action => $action->button()->label('Filters')->slideOver()->closeModalByClickingAway(true))
+            ->filtersFormWidth(Width::Small)
+            ->paginationPageOptions([10, 25, 50, 100]));
 
-        Column::configureUsing(function (Column $column) {
-            return $column->toggleable();
-        });
+        Column::configureUsing(fn(Column $column) => $column->toggleable());
 
-        TextColumn::configureUsing(function (TextColumn $textColumn) {
-            return $textColumn
-                ->searchable()
-                ->sortable();
-        });
+        TextColumn::configureUsing(fn(TextColumn $textColumn) => $textColumn
+            ->searchable()
+            ->sortable());
 
-        Notification::configureUsing(function (Notification $notification) {
-            return $notification->duration(10_000);
-        });
+        Notification::configureUsing(fn(Notification $notification) => $notification->duration(10_000));
     }
 }
--- config/branding.php
+++ config/branding.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Filament\Support\Colors\Color;
 
 return [
